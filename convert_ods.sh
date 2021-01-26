ca07b09da2d54e39909647e36b294e0483546b8a
Obijuan <obijuan.cube@gmail.com>
2021-01-26 10:53:04 +0100

    convert_ods script added

---
+++ convert_ods.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#---- Parameters
+SRC_FILE="report"   #-- orginal .xls file
+TARGET_FILE="listado_orig"  #-- Target ods filename
+
+# ----- Convert the report.xls file to ods format  
+soffice --headless --convert-to ods $SRC_FILE.xls
+
+# ----- Generate the target file
+mv $SRC_FILE.ods $TARGET_FILE.ods
+
+
